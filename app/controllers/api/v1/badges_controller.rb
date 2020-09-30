0b302d718cf0cd419034c1d45a57fd4e7d5c72b7
rayvaldez <ray_valdez@hotmail.co.uk>
2020-09-30 20:21:57 +0100

    Add methods to Badges Controller

--- app/controllers/api/v1/badges_controller.rb
+++ app/controllers/api/v1/badges_controller.rb
@@ -1,2 +1,30 @@
-class BadgesController < ApplicationController
+class Api::V1::BadgesController < ApplicationController
+
+  def index
+    badges = Badge.find(params[:player_id])
+
+    render json: badges, status:200
+  end
+
+  def show
+    badges = Badge.find(params[:id])
+
+    render json: badges, status:200
+  end
+
+  def update
+    player = Player.find(params[:id])
+    badges = Badge.find(params[:id])
+
+    player.badges.update(badge_params)
+
+    render json: player, status:200
+  end
+
+  private
+
+  def badge_params
+    params.require(:badge).permit(:player_id, :performance_buzz, :media_buzz, :capital_appreciation, :prime_time, :twilight_years, :badge)
+  end
+  
 end
