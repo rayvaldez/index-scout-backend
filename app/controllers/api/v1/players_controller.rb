38fcd979557c89751b745aa6d72f6525c1b9bf63
rayvaldez <ray_valdez@hotmail.co.uk>
2020-09-30 20:18:34 +0100

    Add methods to Player Controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,15 @@
 class PlayersController < ApplicationController
+  
+  def index
+    players = Player.all
+
+    render json: players, status:200
+  end
+
+  def show
+    player = Player.find(params[:id])
+
+    render json: player, status:200
+  end
+
 end
