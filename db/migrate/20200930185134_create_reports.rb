b879ce1edc25ad74450b4723aaa3aa6dcf078407
rayvaldez <ray_valdez@hotmail.co.uk>
2020-09-30 19:52:17 +0100

    Create Report resource

---
+++ db/migrate/20200930185134_create_reports.rb
@@ -0,0 +1,11 @@
+class CreateReports < ActiveRecord::Migration[6.0]
+  def change
+    create_table :reports do |t|
+      t.text :content
+      t.string :name
+      t.references :player, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
