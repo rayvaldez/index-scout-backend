3752bdb75ce46be8a711a4fbed464ec44bc652ed
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-01 19:17:31 +0100

    Add model relationships and methods

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  has_many :reports
+  has_many :badges
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,3 +1,7 @@
 class Report < ApplicationRecord
   belongs_to :player
+
+  def created_at
+    attributes['created_at'].strftime("%m/%d/%Y %H:%M")
+  end
 end
