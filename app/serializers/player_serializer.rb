d2327c2a1cabb7c41d87cd47b80924bf430f4d34
rayvaldez <ray_valdez@hotmail.co.uk>
2020-10-06 19:49:30 +0100

    Add image attribute to serializer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :age, :team, :position, :price
+  attributes :id, :name, :age, :team, :position, :price, :image
   has_many :reports
   has_many :badges
 end
